46cdf19485421f24fff6179607bade6388a31416
birbish <asthebirdthatflies@gmail.com>
2024-12-07 18:07:19 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,13 @@
 window.addEventListener('DOMContentLoaded', function() {
-  require('disnut.json', function(disnut) {
-    var inputfield = document.getElementById('numberid');
-    var output = document.getElementById('display');
-
-    try {
-      output.innerHTML = disnut.ligma[Number(inputfield.value)];
-    } catch (e) {
-      output.innerHTML = `Invalid number
+  var disnut = require('disnut.json');
+  var inputfield = document.getElementById('numberid');
+  var output = document.getElementById('display');
+  
+  try {
+    output.innerHTML = disnut.ligma[Number(inputfield.value)];
+  } catch (e) {
+    output.innerHTML = `Invalid number
 
 ` + e;
-    }
   });
 });
