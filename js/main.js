1af68871d46a1bf5f5d917e57282e51cc0b007b9
birbish <asthebirdthatflies@gmail.com>
2024-12-07 17:23:13 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,14 @@
+var disnut = require('disnut.json');
+
 window.addEventListener('DOMContentLoaded', function() {
   var inputfield = document.getElementById('numberid');
   var output = document.getElementById('display');
 
-  async function get_text(file) {
-    let x = await fetch(file);
-    let y = await x.text();
-    return y;
-  }
-
   try {
-    output.innerHTML = get_text('/list.txt').split('\n')[Number(inputfield.value)];
+    output.innerHTML = disnut['disnut'][Number(inputfield.value)];
   } catch (e) {
-    output.innerHTML = 'Invalid number';
+    output.innerHTML = `Invalid number
+    
+    ` + e;
   }
 });
