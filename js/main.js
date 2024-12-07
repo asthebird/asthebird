7a6b6c20dfd28919dfbd97820b382d37ecbd902a
birbish <asthebirdthatflies@gmail.com>
2024-12-07 16:41:52 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-//hello der
+var inputfield = document.getElementById('numberid');
+var output = document.getElementById('display');
+
+async function get_text(file) {
+  let x = await fetch(file);
+  let y = await x.text();
+  return y;
+}
+
+try {
+  output.innerHTML = get_text('/list.txt').split('\n')[Number(inputfield.value)];
+} catch (e) {
+  output.innerHTML = 'Invalid number';
+}
