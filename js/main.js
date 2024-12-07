c5f5353ee42e4ede9f793edabd175362fd362c4f
birbish <asthebirdthatflies@gmail.com>
2024-12-07 21:40:04 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,24 @@
+const fs = require('fs'); 
+
 window.addEventListener('DOMContentLoaded', function() {
-  require(['js/disnut'], function(disnut){
     var inputfield = document.getElementById('numberid');
     var output = document.getElementById('display');
+    var text;
+ 
+    fs.readFile('/disnut.txt', 'utf8', (err, data) => { 
+      if (err) { 
+        console.error('Error reading file:', err); 
+        return;
+      }
+
+      const content = data; 
+      text = content;
+    }); 
   
     try {
-      output.innerHTML = disnut.ligma[parseInt(inputfield.value, 10)];
+      var disnut = text.split('\n');
+      output.innerHTML = disnut[parseInt(inputfield.value, 10)];
     } catch (e) {
-      output.innerHTML = 'Invalid number\n\n' + e;
+      output.innerHTML = 'Something went wrong\n\n' + e;
     }
-  });
 });
