d0d51b3658abce14e2f33d9fa7e97e76fc06f528
birbish <asthebirdthatflies@gmail.com>
2024-12-07 19:51:20 +0800

    Update and rename disnut.json to disnut.js

---
+++ js/disnut.js
@@ -0,0 +1,8 @@
+var ligma = [
+    "Ligma", "Sugma/Sucma", "Bofa", "Eatma", "Fugma","Kisma", "Chokonma", "Fondalma", "Stigma", "Tugunma","Slobonma","Cupma","Nibelma","Tipima","Jergma","Bofadese/Bophides","Dragondese","Sugondese","Rubondese","Miphat","Rydon","Sawcon/Saw-Con","Sloberon","Tipodiss","Gulpin","Grababan","Imagine Dragons","Slawbunnies","Penny Trading","Won a pound","Wiremouth","Candice","Dixon","Parody","Europe","Wilma","Ramma","Nuddinyore","Tuov","Wendy’s","CDs","Plant","Tulips","Fitness","Johan","Taygahloo","Cats","Marmalade","Cho Con","Shogun","Sipdiss","Lee Gandhi","Alpaca","Peanut","Rate","Ryzen","Updog","Zamatta","Endma","Landon","Willya","Pudding","Parodies","Leaving","Goblin","Expos","Yankees/Yankon","Boyardee","Paradis","Kenya","Sea of Thieves","Phil","Tulips","Candice","Canyueligue","Roll Over","Indeed","Sakkon","Felfor","Putindese","Landese","Suko","Manyayo","Chewons","Hoodie","Goodies","Vitamin D","Tugboats","The Andes","Sawhtiboss"
+];
+
+/*
+    For SOME reason, using a simple JSON file didn't work as REQUIRE kept interpreting it as a JS file!
+    wth
+*/
