b84312682144a442e511e75e47f8de2e52e6fdcf
birbish <asthebirdthatflies@gmail.com>
2024-12-08 09:28:32 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,12 +6,12 @@ window.addEventListener('DOMContentLoaded', function() {
     fetch('https://asthebird.github.io/list.txt')
         .then(ligst => ligst.text()).then(out => {
             text = out;
-        })
-  
-    try {
-      var disnut = text.split('\n');
-      output.innerHTML = disnut[parseInt(inputfield.value, 10)];
-    } catch (e) {
-      output.innerHTML = 'Something went wrong\n\n' + e;
-    }
+
+            try {
+              var disnut = text.split('\n');
+              output.innerHTML = disnut[parseInt(inputfield.value, 10)];
+            } catch (e) {
+              output.innerHTML = 'Something went wrong\n\n' + e;
+            }
+        });
 });
